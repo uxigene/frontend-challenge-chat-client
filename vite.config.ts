a6ab87718d786ca00f716e491b477b95a84b865e
Eugeniu Marinescu <marinescuevghenii@gmail.com>
2026-06-27 22:52:33 +0200

    feat(Docker): add docker support

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,12 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
+	server: {
+		allowedHosts: true,
+		port: 3001,
+	},
+	preview: {
+		allowedHosts: true,
+		port: 3001,
+	},
 });
